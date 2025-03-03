463da58b2706d6795ea0abf3d92347d31d508675
heezi <themiyakasun.tb@gmail.com>
2025-03-03 14:03:11 +0530

    Sign in user with emailandPassword

--- lib/data/repositories/authentication/authentication_repository.dart
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -44,6 +44,23 @@ class AuthenticationRepository extends GetxController {
   }
 
   //Email and password sign in
+  Future<UserCredential> loginWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      return await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      throw TFirebaseAuthException(e.code).message;
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
 
   //Register
   Future<UserCredential> registerWithEmailandPassword(
---
+++ lib/features/authentication/controller/login/login_controller.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:medicare/common/widgets/loaders.dart';
+import 'package:medicare/data/repositories/authentication/authentication_repository.dart';
+import 'package:medicare/utils/helpers/network_manager.dart';
+import 'package:medicare/utils/popups/full_screen_loader.dart';
+import 'package:medicare/utils/theme/custom_themes/image_strings.dart';
+
+class LoginController extends GetxController {
+  final localStorage = GetStorage();
+  final rememberMe = false.obs;
+  final hidePassword = false.obs;
+  final email = TextEditingController();
+  final password = TextEditingController();
+  GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+
+  // @override
+  // void onInit() {
+  //   email.text = localStorage.read('REMEMBER_ME_EMAIL');
+  //   password.text = localStorage.read('REMEMBER_ME_PASSWORD');
+  //   super.onInit();
+  // }
+
+  Future<void> emailAndPasswordSignIn() async {
+    try {
+      TFullScreenLoader.openLoadingDialog(
+          'Logging you in...', TImages.processing);
+
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+
+      if (rememberMe.value) {
+        localStorage.write('REMEMBER_ME_EMAIL', email.text.trim());
+        localStorage.write('REMEMBER_ME_PASSWORD', password.text.trim());
+      }
+
+      final userCredential = await AuthenticationRepository.instance
+          .loginWithEmailAndPassword(email.text.trim(), password.text.trim());
+
+      TFullScreenLoader.stopLoading();
+
+      AuthenticationRepository.instance.screenRedirect();
+    } catch (e) {
+      TFullScreenLoader.stopLoading();
+      TLoaders.errorSnackBar(title: 'Oh Snap', message: e.toString());
+    }
+  }
+}
